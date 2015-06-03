b438f4c06a4d1b9640ad078304435df122ec38f3
Quinn <mqparkinson@gmail.com>
2015-06-03 00:43:28 -0700

    Trying to fix an error

--- blog/views.py
+++ blog/views.py
@@ -20,7 +20,7 @@ def post_new(request):
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = None
+            post.author = request.user
             post.save()
             post.publish()
             return redirect('blog.views.post_detail',pk=post.pk)
