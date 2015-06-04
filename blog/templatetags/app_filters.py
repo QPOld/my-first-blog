cbf13a6cb91a2ed0bdf0b12aeba8f5bb3fe49163
Quinn <mqparkinson@gmail.com>
2015-06-04 14:33:38 -0700

    added audio

--- blog/templatetags/app_filters.py
+++ blog/templatetags/app_filters.py
@@ -1,2 +1,21 @@
 from django import template
+from django.template.defaultfilters import stringfilter
 register = template.Library()
+
+import soundcloud
+
+
+register = template.Library()
+
+@register.filter()
+@stringfilter
+def SCURL(string):
+    # # create a client object with your app credentials
+    client = soundcloud.Client(client_id='YOUR_CLIENT_ID')
+    #
+    # # get a tracks oembed data
+    track_url = str(string)
+    embed_info = client.get('/oembed', url=track_url)
+    #
+    # # print the html for the player widget
+    return embed_info.html
